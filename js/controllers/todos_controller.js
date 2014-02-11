7c65361f1b890d2cc6c01271cf34b89805b46b25
Joe Ngo <joengo83@gmail.com>
2014-02-11 00:22:38 -0800

    displaying the number of completed tasks

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,6 +1,6 @@
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
-    createTodo: function() {
+    createTodo: function () {
       // Get the todo title set by the "New Todo" text field
       var title = this.get('newTitle');
       if (!title.trim()) { return; }
@@ -17,6 +17,15 @@ Todos.TodosController = Ember.ArrayController.extend({
       // Save the new model
       todo.save();
     }
-  }
+  },
+
+  remaining: function () {
+    return this.filterProperty('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function () {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining')
 });
 
