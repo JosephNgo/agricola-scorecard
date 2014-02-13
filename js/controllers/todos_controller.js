7b9c3f035a7579678cac6487d89b9c4f72515fed
Joe Ngo <joengo83@gmail.com>
2014-02-12 23:26:18 -0800

    indicating when all tasks are complete

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -25,6 +25,10 @@ Todos.TodosController = Ember.ArrayController.extend({
     }
   },
 
+  allAreDone: function() {
+    return !!this.get('length') && this.everyProperty('isCompleted', true);
+  }.property('@each.isCompleted'),
+
   hasCompleted: function() {
     return this.get('completed') > 0;
   }.property('completed'),
